5f35d8ba17eac3f19556b58f54985d1bfc7cfd56
Elliot <elliot.james.williams@gmail.com>
2017-11-04 14:22:22 -0400

    Added base bash script; python script it interfaces with to process the PDB files

---
+++ code/processor.py
@@ -0,0 +1,21 @@
+# PYTHON 3
+import sys
+
+if len(sys.argv) < 2:
+    print("ERROR: No filename(s) specified. Try again, specifying file(s) to parse")
+    sys.exit()
+
+pdb_to_parse = sys.argv[1:]
+
+# pdb_to_parse = ["../data/pdb_input" + f for f in pdb_to_parse]
+
+print(pdb_to_parse)
+
+
+for pdb_filename in pdb_to_parse:
+    # Opens file; line buffering
+    print("Opening " + pdb_filename)
+    filedata = open(pdb_filename, "r", 1)
+
+    for line in filedata:
+        # print(line)
---
+++ code/script.sh
@@ -0,0 +1,20 @@
+# !#/bin/bash
+# Base script which runs python scripts therein
+
+
+
+# Gets into relative directory where script lives
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+    pwd
+cd ../data/pdb_input
+
+# Checks for pdb_input files,
+# then runs our conversion python script on them
+for i in $( ls )
+do
+  echo $i #TODO: Run python script for all of these
+done
+
+cd ../../code/
+
+/usr/bin/python3 processor.py ../data/pdb_input/*.pdb
