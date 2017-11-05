371f542f6c6d9068546119e01648681d9604fb17
Elliot <elliot.james.williams@gmail.com>
2017-11-04 20:45:26 -0400

    Added file writing code

--- code/processor.py
+++ code/processor.py
@@ -1,11 +1,12 @@
 # PYTHON 3
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print("ERROR: No filename(s) specified. Try again, specifying file(s) to parse")
     sys.exit()
 
-pdb_to_parse = sys.argv[1:]
+nn = sys.argv[1]
+pdb_to_parse = sys.argv[2:]
 
 # pdb_to_parse = ["../data/pdb_input" + f for f in pdb_to_parse]
 
@@ -16,6 +17,8 @@ for pdb_filename in pdb_to_parse:
     # Opens file; line buffering keeps it efficient and not slow, for large pdbs
     print("Opening " + pdb_filename)
     filedata = open(pdb_filename, "r", 1)
+    path = "../pdb_output/"
+    new_file = open(path+pdb_filename[0:len(pdb_filename)-4]+"_"+nn+".pdb" , "w")
 
     current_atom_number = 1
     current_chain = "CURRENTLY NULL"
@@ -25,6 +28,7 @@ for pdb_filename in pdb_to_parse:
         is_atom = atom_string == "ATOM"
         if is_atom:
             if chain_name != current_chain:
+                new_file.write(chain_name + "\n")
                 print("Last number: " + str(current_atom_number))
                 current_chain = chain_name
                 print("New chain: "   + str(current_chain))
@@ -34,3 +38,4 @@ for pdb_filename in pdb_to_parse:
     # This happens automatically,
     # but it's good practice -- in case I add more stuff
     filedata.close()
+    new_file.close()
