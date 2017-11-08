a476b408e6122b8710aea18606c845e5c92d76f5
Elliot <elliot.james.williams@gmail.com>
2017-11-08 18:14:13 -0500

    Removed spurious printing from script

--- script.sh
+++ script.sh
@@ -5,15 +5,15 @@
 
 # Gets into relative directory where script lives
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-    pwd
+    # pwd
 cd ./data/pdb_input
 
 # Checks for pdb_input files,
 # then runs our conversion python script on them
-for i in $( ls )
-do
-  echo $i #TODO: Run python script for all of these
-done
+# for i in $( ls )
+# do
+#   echo $i #TODO: Run python script for all of these
+# done
 
 cd ../../code/
 
