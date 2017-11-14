1aa869172dfa7a5b8bec6084a556ecb828228be0
Elliot <elliot.james.williams@gmail.com>
2017-11-14 14:54:02 -0500

    Fixed formatting; added CONECT error formatting type

--- code/checkForInconsistencies.py
+++ code/checkForInconsistencies.py
@@ -4,34 +4,35 @@ import sys
 pdb_file1 = sys.argv[1]
 pdb_file2 = sys.argv[2]
 
+print("\n>>>>>Checking for PDB Inconsistencies Script Running")
 
-'''
-Input: filename for which we're trying to calculate
-Output: number of lines in each file
-'''
-def file_len(file):
-    with file as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+path = "../data/pdb_output/"
 
 print("Opening " + pdb_file1)
-path = "../data/pdb_output/"
-# filedata = open(path + pdb_file1, "r", 1)
 pdb1 = open(path + pdb_file1 , "r+")
+print("Opening " + pdb_file2)
 pdb2 = open(path + pdb_file2 , "r+")
 
 pdb1_lines = pdb1.readlines()
 pdb2_lines = pdb2.readlines()
 
-print("The reference file has : " + str(len(pdb1_lines)) + " lines")
-print("The produced  file has : " + str(len(pdb2_lines)) + " lines")
+print("The reference file (1) has : " + str(len(pdb1_lines)) + " lines")
+print("The produced  file (2) has : " + str(len(pdb2_lines)) + " lines")
 
+
+
+num_different = 0
 for i in range(min(len(pdb1_lines), len(pdb2_lines))):
     if pdb1_lines[i].strip() == pdb2_lines[i].strip():
         pass
     else:
-        print("\nLine Number is : " + str(i+1))
-        print("1>>" + pdb1_lines[i])
-        print("2>>" + pdb2_lines[i])
-        exit()
+        if pdb1_lines[i].split()[0] == "CONECT":
+            print(">>Error on CONECT @ line " + str(i+1))
+        else:
+            print(">>>> Error " + str(num_different+1))
+            print("Line Number is : " + str(i+1))
+            print("1>>" + pdb1_lines[i].strip())
+            print("2>>" + pdb2_lines[i].strip() + "\n")
+            num_different += 1
+            if num_different is 10:
+                exit()
